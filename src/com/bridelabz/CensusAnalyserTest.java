251ee1751844e5b38e43f2399d1610f3c3e09d5d
9663461085 <ratansollapure16310@gmail.com>
2023-01-11 12:37:37 +0530

    Added:Indian State Analyser Programm

---
+++ src/com/bridelabz/CensusAnalyserTest.java
@@ -0,0 +1,19 @@
+package com.bridelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+	private static final String INDIA_CENSUS_CSV_FILE_PATH = "E:\\RFP229\\CensusAnalyser\\IndiaStateCensusData.csv";
+
+	@Test
+	public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+		try {
+			CensusAnalyser censusAnalyser = new CensusAnalyser();
+			int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+			Assert.assertEquals(29, numOfRecords);
+		} catch (CensusAnalyserException e) {
+		}
+	}
+
+}
